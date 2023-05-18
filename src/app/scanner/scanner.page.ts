500103176cd783e58452c422f68a40c702d83086
MarcAtBitdynamics <129751845+MarcAtBitdynamics@users.noreply.github.com>
2023-05-18 14:39:20 -0300

    t

--- src/app/scanner/scanner.page.ts
+++ src/app/scanner/scanner.page.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
-import { AlertController } from '@ionic/angular';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { Platform } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import {
+  BarcodeScanner,
+  BarcodeScannerOptions,
+} from "@ionic-native/barcode-scanner/ngx";
+import { AlertController } from "@ionic/angular";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { Platform } from "@ionic/angular";
 import { Location } from "@angular/common";
-import { Storage } from '@ionic/storage';
+import { Storage } from "@ionic/storage";
 
 @Component({
-  selector: 'app-scanner',
-  templateUrl: './scanner.page.html',
-  styleUrls: ['./scanner.page.scss'],
+  selector: "app-scanner",
+  templateUrl: "./scanner.page.html",
+  styleUrls: ["./scanner.page.scss"],
 })
 export class ScannerPage implements OnInit {
   scanData: {};
   status: any;
+
   constructor(
     private barcodeScanner: BarcodeScanner,
     private AlertController: AlertController,
@@ -23,185 +27,193 @@ export class ScannerPage implements OnInit {
     private location: Location,
     public http: HttpClient,
     private storage: Storage
-
   ) {
     this.platform.backButton.subscribeWithPriority(666666, () => {
       if (this.router.url == "/scanner") {
-        this.exitalert()
+        this.exitalert();
       } else {
         this.location.back();
       }
-    })
+    });
   }
 
   ngOnInit() {
-    // this.http.get('http://192.168.0.159:8080/api/get-status')
-    // this.http.get('http://mia.bitdynamics.sr/sslapp/api/get-status')
-    this.http.get('https://ssl.app.sr/api/get-status')
-      .subscribe(async Response => {
+    this.http.get("https://ssl.app.sr/api/get-status").subscribe(
+      async (Response) => {
         console.log(Response);
         this.storage.set("status", Response);
-      }, async error => {
+      },
+      async (error) => {}
+    );
 
-      });
-    this.storage.get("currentStatus").then((val) => {
-      this.status = val;
-      console.log(val)
-    })
+    this.status = localStorage.getItem("currentStatus");
   }
   ionViewDidEnter() {
-    this.storage.get("currentStatus").then((val) => {
-      this.status = val;
-      console.log(val)
-    })
+    this.status = localStorage.getItem("currentStatus");
+    console.log(this.status);
   }
+
+ 
+  
   scan() {
-    this.storage.get("currentStatus").then((val) => {
-      if (val == null) {
-        this.setStatus();
-      }
-      if (val != null) {
-        const options: BarcodeScannerOptions = {
-          preferFrontCamera: false,
-          showFlipCameraButton: false,
-          prompt: 'Place a barcode inside the scan area',
-          formats: 'CODE_93',
-          orientation: 'portrait',
-        };
-
-        this.barcodeScanner.scan(options).then((barcodeData) => {
+    console.log("scan called");
+    const storedStatus = localStorage.getItem("selectedStatus");
+    const currentStatus = storedStatus ? JSON.parse(storedStatus) : null;
+    
+    console.log("currentStatus from storage:", currentStatus);
+    this.status = currentStatus;
+  
+    if (currentStatus == null) {
+      console.log("currentStatus is null");
+      this.setStatus();
+    } else {
+      const options: BarcodeScannerOptions = {
+        preferFrontCamera: false,
+        showFlipCameraButton: false,
+        prompt: "Place a barcode inside the scan area",
+        formats: "CODE_93",
+        orientation: "portrait",
+      };
+  
+      this.barcodeScanner.scan(options)
+        .then((barcodeData) => {
           console.log(barcodeData);
-          console.log(barcodeData.text);
-          if (!barcodeData.text) {
-            console.log("barcodeData.text is undefined");
-            return;
-          }
-          if(barcodeData.text != ""){
-            this.postBarcodeData(barcodeData.text)
-          }
-         
-          this.scanData = barcodeData;
-        }, (err) => {
-          console.log("Error occured : " + err);
+          this.postBarcodeData(barcodeData.text);
+        })
+        .catch((err) => {
+          console.log("Error", err);
         });
-      }
-    })
-
+    }
   }
   async alert(Response) {
     const alert = await this.AlertController.create({
-      header: 'Scan succesvol',
-      cssClass: 'alertCss',
-      message: Response.pakket_id + ' status is gewijzigd ' + Response.name,
-      buttons: [{
-        text: 'Scan volgende',
-        handler: () => {
-          this.scan()
-        }
-      },
-      {
-        text: 'ok',
-        handler: () => {
-          console.log('Yes clicked');
-
-        }
-      }]
+      header: "Scan succesvol",
+      cssClass: "alertCss",
+      message: Response.pakket_id + " status is gewijzigd " + Response.name,
+      buttons: [
+        {
+          text: "Scan volgende",
+          handler: () => {
+            this.scan();
+          },
+        },
+        {
+          text: "ok",
+          handler: () => {
+            console.log("Yes clicked");
+          },
+        },
+      ],
     });
     await alert.present();
   }
 
-  postBarcodeData(data) {
-    let barcode = {
-      "barcode": data,
-      "status": this.status.id
-    }
-    console.log(barcode)
-    //  this.http.post('http://192.168.0.159:8080/api/barcode-update-status', barcode)
-    // this.http.post('http://mia.bitdynamics.sr/sslapp/api/barcode-update-status', barcode)
-    this.http.post('https://ssl.app.sr/api/barcode-update-status', barcode)
-    
-      .subscribe(async Response => {
-        console.log(Response);
-        // this.loadingController.dismiss();
-        console.log("weee")
-        this.alert(Response[0]);
-      }, async error => {
-        this.sameStatusAlert(error)
-        console.log(error)
-      });
+  
 
+  postBarcodeData(data: any) {
+    let barcode = {
+      barcode: data,
+      // status: this.status && this.status.id ? this.status.id : null,
+      status: this.status.id,
+    };
+    console.log("Posting barcode data: ", barcode);
+
+    this.http
+      .post("https://ssl.app.sr/api/barcode-update-status", barcode)
+      .subscribe(
+        async (response) => {
+          console.log("response :", response);
+          if (Array.isArray(response) && response.length > 0) {
+            // check if the response is an array with at least one element
+            console.log("weee");
+            this.alert(response[0]); // call the alert function with the first element of the response array
+          } else {
+            console.log("Invalid response format: ", response); // print an error message to the console
+          }
+        },
+        async (error) => {
+          this.sameStatusAlert(error); // call the sameStatusAlert function with the error object
+          console.log("Error: ", error);
+        }
+      );
   }
 
   pakketten() {
-    this.router.navigateByUrl('/pakketten');
+    this.router.navigateByUrl("/pakketten");
   }
   settings() {
-    this.router.navigateByUrl('/settings');
+    this.router.navigateByUrl("/settings");
   }
   async exitalert() {
     const alert = await this.AlertController.create({
       // header: 'Registratie Successvol',
-      header: 'Wilt u de applicatie verlaten?',
-      cssClass: 'alertCss',
+      header: "Wilt u de applicatie verlaten?",
+      cssClass: "alertCss",
       // message: 'U zal een verificatie ontvangen in uw mail',
-      buttons: [{
-        text: 'Ja',
-        handler: () => {
-          navigator["app"].exitApp();
-        }
-      },
-      {
-        text: 'Nee',
-        handler: () => {
-          console.log('Yes clicked');
-
-        }
-      }]
+      buttons: [
+        {
+          text: "Ja",
+          handler: () => {
+            navigator["app"].exitApp();
+          },
+        },
+        {
+          text: "Nee",
+          handler: () => {
+            console.log("Yes clicked");
+          },
+        },
+      ],
     });
     await alert.present();
   }
 
   async setStatus() {
     const alert = await this.AlertController.create({
-      header: 'Er is geen status geselecteerd',
-      // header: 'Selecteer eerst een status',
-      // message: 'Er is geen status geselecteerd',
-      cssClass: 'alertCss',
-      buttons: [{
-        text: 'Selecteer status',
-        handler: () => {
-          this.router.navigateByUrl('/settings');
-        }
-      },
-      {
-        text: 'annuleer',
-        handler: () => {
-          console.log('Yes clicked');
+      header: "Er is geen status geselecteerd",
 
-        }
-      }]
+      cssClass: "alertCss",
+      buttons: [
+        {
+          text: "Selecteer status",
+          handler: () => {
+            this.router.navigateByUrl("/settings");
+          },
+        },
+        {
+          text: "annuleer",
+          handler: () => {
+            console.log("Yes clicked");
+          },
+        },
+      ],
     });
     await alert.present();
   }
-  async sameStatusAlert(Response) {
-    const alert = await this.AlertController.create({
-      header: 'Scan niet succesvol',
-      message: Response.error.message,
-      cssClass: 'alertCss',
-      buttons: [{
-        text: 'Scan volgende',
-        handler: () => {
-          this.scan()
-        }
-      },
-      {
-        text: 'ok',
-        handler: () => {
-          console.log('Yes clicked');
 
-        }
-      }]
-    });
-    await alert.present();
+ 
+  async sameStatusAlert(Response) {
+    if (Response.error) {
+      const alert = await this.AlertController.create({
+        header: "Scan niet succesvol",
+        message: Response.error.message,
+        cssClass: "alertCss",
+        buttons: [
+          {
+            text: "Scan volgende",
+            handler: () => {
+              this.scan();
+            },
+          },
+          {
+            text: "ok",
+            handler: () => {
+              console.log("Yes clicked");
+            },
+          },
+        ],
+      });
+      await alert.present();
+    }
   }
 }
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,73 +1,233 @@
-import { Component, OnInit } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import { Router } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Storage } from "@ionic/storage";
+import { Router, ActivatedRoute } from "@angular/router";
+
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+  selector: "app-settings",
+  templateUrl: "./settings.page.html",
+  styleUrls: ["./settings.page.scss"],
 })
 export class SettingsPage implements OnInit {
+  // Variables declaration
   name: any;
   email: any;
   role: any;
   status: any;
   mode: any;
-  currentStatus: any;
+  locatie: any;
+  currentStatus: any = null;
+  showFields: boolean;
+  showPalletiseren: boolean;
+  selectedStatus: any;
+  currentPalletNumber: string;
+  palletNumber: any;
+  statusList: any[] = [];
+  saveButtonClicked = false;
+  selectedStatusId : number;
+  parsedStatus :any;
+  
+  
+   // Show palletiseren fields
+  showPalletiserenFields() {
+    this.showFields = true;
+  }
+  // Hide palletiseren fields
+  hidePalletiserenFields() {
+    this.showFields = false;
+  }
 
-  constructor(private Storage: Storage,
-    private Router: Router) { }
+  constructor(
+    private Storage: Storage,
+    private router: Router,
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit() {
-    this.Storage.get('login').then((data) => {
+  ngOnInit() { 
+
+    // const isFirstOpen = localStorage.getItem("isFirstOpen");
+
+    // if (!isFirstOpen) {
+    //   // Clear the stored values from local storage
+    //   localStorage.removeItem("login");
+    //   localStorage.removeItem("status");
+    //   localStorage.removeItem("selectedStatus");
+    //   localStorage.removeItem("currentStatus");
+    //   localStorage.removeItem("palletNumber");
+  
+    //   // Set the flag to indicate that the application has been opened
+    //   localStorage.setItem("isFirstOpen", "true");
+    // }
+
+
+
+    // Get the stored values from the local storage
+    this.Storage.get("login").then((data) => {
       this.name = data.name;
       this.email = data.email;
       this.role = data.role;
-    })
-    this.Storage.get("status").then((val) => {
-      this.status = val;
-    })
-    this.Storage.get("currentStatus").then((val) => {
-      if (val != null) {
-        this.currentStatus = val.name;
-
+      this.locatie = data.locatie;
+    });
+  
+    this.Storage.get("status").then((response) => {
+      if (response && response.status) {
+        this.statusList = Object.values(response.status);
       }
-    })
-    this.Storage.get("mode").then((val) => {
-      // this.currentStatus = val.name;
-      this.mode = val;
-    })
-
-    if (this.currentStatus == null) {
-      this.currentStatus = "Selecteer status"
-    }
-  }
-  onChangeStatus($event) {
-
-    this.status.forEach((value) => {
-      if (value.id == $event.detail.value) {
-        this.Storage.set("currentStatus", value)
+    });
+  
+    // Get the current status from the query parameter
+    this.activatedRoute.queryParams.subscribe((params) => {
+      const statusId = params["statusId"];
+      const storedStatus = localStorage.getItem("selectedStatus");
+  
+      if (statusId) {
+        const selectedStatus = this.statusList.find((status) => status.id === +statusId);
+        if (selectedStatus) {
+          this.currentStatus = selectedStatus;
+          this.selectedStatusId = selectedStatus.id;
+          localStorage.setItem("selectedStatus", JSON.stringify(this.currentStatus));
+          localStorage.setItem("currentStatus", JSON.stringify(this.currentStatus));
+          console.log("The status has been saved:", this.currentStatus);
+        }
+      } else if (storedStatus) {
+        this.currentStatus = JSON.parse(storedStatus);
+        this.selectedStatusId = this.currentStatus ? this.currentStatus.id : null;
+        console.log("The status has been retrieved from browser storage:", this.currentStatus);
+      }
+  
+      // Retrieve the stored pallet number from browser storage
+      const storedPalletNumber = localStorage.getItem("palletNumber");
+      if (this.currentStatus && storedPalletNumber) {
+        this.currentStatus.palletNumber = storedPalletNumber;
+        console.log("The pallet number is:", storedPalletNumber);
       }
     });
   }
+  
+
+
+
+  
+  onChangeStatus() {
+  this.saveButtonClicked = false;
+  try {
+    if (this.selectedStatusId) {
+      const selectedStatus = this.statusList.find((status) => status.id === this.selectedStatusId);
+      if (selectedStatus) {
+        this.currentStatus = selectedStatus;
+        // Update the query parameter with the selected status
+        const queryParams = this.currentStatus ? { statusId: this.currentStatus.id } : {};
+        this.router.navigate([], {
+          queryParams,
+          queryParamsHandling: "merge",
+        });
+
+        // Save the selected status in browser storage
+        localStorage.setItem("currentStatus", JSON.stringify(this.currentStatus));
+        console.log(this.currentStatus);
+        
+       // localStorage.setItem("selectedStatus", JSON.stringify(this.currentStatus));
+
+        if (this.currentStatus.name === "Palletiseren") {
+          console.log("palletiseren has been selected");
+          this.showFields = true;
+          this.currentStatus.palletNumber = this.currentPalletNumber;
+        } else {
+          console.log("palletiseren has not been selected");
+          this.showFields = false;
+        }
+      } else {
+        // Placeholder option selected, set currentStatus to null
+        this.currentStatus = null;
+        // Hide the palletiseren fields or perform any other necessary actions
+        this.router.navigate([], {
+          queryParams: { statusId: null }, // Update the query parameter to null
+          queryParamsHandling: "merge",
+        });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+  
+
+
+  async onSavePalletNumber() {
+    const apiUrl = "https://ssl.app.sr/api/save-pallet";
+
+    if (typeof this.currentStatus === "object") {
+      // Save the pallet number in the currentStatus object
+      this.currentStatus.palletNumber = this.palletNumber;
+      const palletNumber = this.palletNumber;
+
+      // Check if the pallet number is empty
+      if (!palletNumber) {
+        alert("Pallet number is empty. Please enter a valid pallet number.");
+        return; // Stop further execution
+      }
+
+      // Get the stored values from the local storage
+      const data = await this.Storage.get("login");
+
+      // Include the location data in the payload
+      const playLoad = {
+        pallet_number: palletNumber,
+        Locatie: data.locatie,
+      };
+
+      // Check if the button has been clicked before saving data
+      if (this.saveButtonClicked) {
+        this.http
+          .post(apiUrl, playLoad)
+          .toPromise()
+          .then((data) => {
+            console.log("Pallet number is saved successfully");
+
+
+            // save the currentStatus and palletNumber in browser storage
+
+            localStorage.setItem("selectedStatus",JSON.stringify(this.currentStatus));
+            localStorage.setItem("palletNumber",this.currentStatus.palletNumber);
+
+
+            this.palletNumber = ""; // Reset the input value
+            this.saveButtonClicked = false; // Reset the flag
+          })
+          .catch((error) => {
+            console.log("Error saving pallet number: ", error);
+          });
+      } else {
+        console.log("Button has not been clicked yet, data not saved.");
+      }
+    }
+  }
+
+  onClearPalletNumber() {
+    this.palletNumber = '';
+    this.currentStatus.palletNumber = ''; // Clear the pallet number in the currentStatus object
+  }
+
   toggleDarkMode(event) {
-    this.Storage.set("mode", event.detail.checked)
+    this.Storage.set("mode", event.detail.checked);
     if (event.detail.checked == true) {
-      document.body.setAttribute('color-theme', 'dark');
+      document.body.setAttribute("color-theme", "dark");
     } else {
       // document.body.setAttribute('color-theme', 'light');
-      document.body.setAttribute('color-theme', 'light2');
+      document.body.setAttribute("color-theme", "light2");
     }
   }
   logout() {
-    this.Storage.clear()
-    this.Router.navigateByUrl('/login')
+    localStorage.removeItem("login");
+    localStorage.removeItem("status");
+    localStorage.removeItem("selectedStatus");
+    localStorage.removeItem("currentStatus");
+    localStorage.removeItem("palletNumber");
+    console.log("Local storage cleared.");
+    this.router.navigateByUrl("/login");
+   
   }
-
-  // toggle(ev){
-  //   if (ev.detail.checked == true) {
-  //     document.body.setAttribute('color-theme', 'tolight');
-  //   }
-  // }
-
 }
