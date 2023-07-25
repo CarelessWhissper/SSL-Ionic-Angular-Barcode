99a18c9e3e2f2fc55b594c53dd91fe8c85c36791
MarcAtBitdynamics <129751845+MarcAtBitdynamics@users.noreply.github.com>
2023-07-25 08:53:03 -0300

    final build 1.1

--- src/app/pakketten/pakketten.page.ts
+++ src/app/pakketten/pakketten.page.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import {
-  LoadingController,
-  AlertController,
-  PopoverController,
-} from "@ionic/angular";
+import { LoadingController, AlertController, PopoverController } from "@ionic/angular";
 import { Storage } from "@ionic/storage";
 import { SortingOptionsComponent } from "./SortingOptionsComponent";
 
@@ -30,7 +26,9 @@ export class PakkettenPage implements OnInit {
     await this.loadData();
   }
 
+  // Load packages data from the API
   async loadData() {
+    // Show loading spinner while loading data
     const loading = await this.loadingController.create({
       message: "Pakketten worden geladen...",
     });
@@ -46,7 +44,7 @@ export class PakkettenPage implements OnInit {
         .get<any[]>("https://ssl.app.sr/api/packages", { params })
         .toPromise();
       this.pakketten = response;
-      this.filteredPakketten = response;
+      this.filteredPakketten = response; // Initially set filteredPakketten to all packages
     } catch (error) {
       console.log("Error retrieving packages:", error);
     } finally {
@@ -54,6 +52,7 @@ export class PakkettenPage implements OnInit {
     }
   }
 
+  // Get the location from storage
   async getLocation(): Promise<string> {
     const data = await this.storage.get("login");
     const locatie = data ? data.locatie : null;
@@ -67,35 +66,41 @@ export class PakkettenPage implements OnInit {
     }
   }
 
-  onChangeSearch() {
+  // Handle the search input change event
+  async onChangeSearch() {
     console.log("Search input:", this.input);
     if (this.input && this.input.trim().length >= 5) {
-      this.filteredPakketten = this.pakketten.filter((pakket) => {
-        const pakketId = pakket.pakket_id.toLowerCase();
-
-        const input = this.input.toLowerCase().trim();
-        return pakketId.includes(input);
-      });
+      // Filter packages using the API
+      await this.filterPackages();
     } else {
       this.filteredPakketten = this.pakketten;
     }
   }
 
+  // Filter packages based on the search input using the API
   async filterPackages() {
+    console.log("Filtering packages...");
     const loading = await this.loadingController.create({
       message: "Zoeken...",
     });
     await loading.present();
-  
+
     try {
-      const location = await this.getLocation();
+      const searchInput = this.input.trim();
+
       const params = {
-        search: this.input.trim(), // Pass the search input to the API
-        location: location,
+        search: searchInput,
       };
-      const response = await this.http.get<any[]>("https://ssl.app.sr/api/display", { params }).toPromise();
-      console.log("Search input:", this.input.trim());
+
+      console.log("Filtering with params:", params); // Log the params being used for the API request
+
+      const response = await this.http
+        .get<any[]>("https://ssl.app.sr/api/display", { params })
+        .toPromise();
+
+      console.log("Search input:", searchInput);
       console.log("Filtered packages:", response);
+
       this.filteredPakketten = response;
     } catch (error) {
       console.log("Error filtering packages:", error);
@@ -103,7 +108,8 @@ export class PakkettenPage implements OnInit {
       await loading.dismiss();
     }
   }
-  
+
+  // Show a confirmation dialog to change the package status
   async showConfirmationDialog(id: string, status_id: number) {
     const pakket = this.pakketten.find((pakket) => pakket.id === id);
     const pakket_id = pakket ? pakket.pakket_id : id;
@@ -127,6 +133,7 @@ export class PakkettenPage implements OnInit {
     await alert.present();
   }
 
+  // Change the package status using the API
   async doChangeStatus(paketId: string, status_id: number): Promise<boolean> {
     const data = {
       id: paketId,
@@ -154,6 +161,7 @@ export class PakkettenPage implements OnInit {
     }
   }
 
+  // Show the sorting options popover
   async presentSortingOptions() {
     const popover = await this.popoverController.create({
       component: SortingOptionsComponent,
@@ -171,6 +179,7 @@ export class PakkettenPage implements OnInit {
     await popover.present();
   }
 
+  // Handle the selected sorting option
   handleSortingOption(option: string) {
     switch (option) {
       case "status_name":
@@ -190,6 +199,7 @@ export class PakkettenPage implements OnInit {
     }
   }
 
+  // Sort packages by status name
   sortByStatusName() {
     this.sortPackages((a, b) => {
       const statusA = (a.status_name || "").toUpperCase();
@@ -198,6 +208,7 @@ export class PakkettenPage implements OnInit {
     });
   }
 
+  // Sort packages by package ID
   sortByPakketId() {
     this.sortPackages((a, b) => {
       const pakketIdA = (a.pakket_id || "").toUpperCase();
@@ -205,6 +216,8 @@ export class PakkettenPage implements OnInit {
       return pakketIdA.localeCompare(pakketIdB);
     });
   }
+
+  // Sort packages by sender
   sortByVerzender() {
     this.sortPackages((a, b) => {
       const verzenderA = (a.verzender || "").toUpperCase();
@@ -212,6 +225,8 @@ export class PakkettenPage implements OnInit {
       return verzenderA.localeCompare(verzenderB);
     });
   }
+
+  // Sort packages by recipient
   sortByOntvanger() {
     this.sortPackages((a, b) => {
       const ontvangerA = (a.ontvanger || "").toUpperCase();
@@ -220,22 +235,27 @@ export class PakkettenPage implements OnInit {
     });
   }
 
+  // Sort packages based on the provided compare function
   sortPackages(compareFn: (a: any, b: any) => number) {
     this.filteredPakketten.sort(compareFn);
   }
 
+  // Toggle showing package details
   toggleDetails(pakket: any) {
     pakket.showDetails = !pakket.showDetails;
   }
 
+  // Get the eye icon based on whether details are shown or not
   getEyeIcon(pakket: any): string {
     return pakket.showDetails ? "eye-off" : "eye";
   }
 
+  // Reload packages data
   reloadPackages() {
-    this.loadData(); // Re-fetch the packages data
+    this.loadData();
   }
 
+  // Show an alert with the provided message
   async presentAlert(message: string) {
     const alert = await this.alertController.create({
       message: message,
